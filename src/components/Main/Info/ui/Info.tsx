29b3edb44f5e3b344c0e67d67979cd396f3db4f8
Ivan <vany4golos@yandex.ru>
2025-04-07 16:35:23 +0200

    fix: MobileAdaptive

--- src/components/Main/Info/ui/Info.tsx
+++ src/components/Main/Info/ui/Info.tsx
@@ -9,31 +9,31 @@ interface IInfoData {
 export const Info = ({ className }: IInfoData) => {
   return (
     <section className={classNames(cls.section, {}, [className || ''])}>
-      <div className={classNames(cls.container, {}, [])}>
+      <article className={classNames(cls.container, {}, [])}>
+        <p className={classNames(cls.subtitle, {}, [])}>Студент</p>
         <div className={classNames(cls.info, {}, [])}>
-          <div className={classNames(cls.imageWrap, {}, [])}>
-            <img 
-              className={classNames(cls.avatar, {}, [])}
-              src={AvatarImage}
-              alt='Аватар создателя'
-            />
-          </div>
-          <h2 className={classNames(cls.heading, {}, [])}>
-            Обо мне!
+          <h2 className={classNames(cls.name, {}, [])}>
+            Чернышев Иван
           </h2>
-          <p className={classNames(cls.subtitle, {}, [])}>
-            Меня зовут Ваня, я студент КФУ им. Вернадского,
-            4 курса, факультета Прикладной математики. Этот проект 
-            является моей дипломной работой, где с помощью знаний набранные 
-            на занятиях, а также самостоятельно изучая разные источники, создал
-            веб приложение, которое состовляет расписание для учебного заведения.
-          </p>
-          <p className={classNames(cls.subtitle, {}, [])}>
-            В этом проекте используется как и front-end, так и back-end, оба
-            написанные на Js и NodeJs. Используются фрейморк React, и сборщик Vite.
-          </p>
+          <h3 className={classNames(cls.work, {}, [])}>
+            студент КФУ, ПМ-211
+          </h3>
+          <p className={classNames(cls.description, {}, [])}>
+          Я родился в Крыму, городе Симферополь. Учусь на 3 курсе, направлении
+          Прикладная математика. В детстве когда у нас в семье появился компьютер а в нем компьютерные игры, я погрузился
+          в мир ноликов и едениц. Но с кодингом я познакомился только в 9 классе, когда готовился к экзаменам и для меня этот виртуальный мир
+          стал еще шире и прикольнее. Люблю кодить, люблю web, люблю когда все надо фиксить, люблю когда все ломается. I LOVE THIS WORLD!</p>
         </div>
-      </div>
+        <a 
+          target='_blank' 
+          href="https://github.com/VanyaGachist2" 
+          className={classNames(cls.link, {}, [])}>GitHub</a>
+        <img 
+          alt="аватарка пользователя данной страницы"
+          src={AvatarImage} 
+          className={classNames(cls.avatar, {}, [])}
+        />
+      </article>
     </section>
   );
 };

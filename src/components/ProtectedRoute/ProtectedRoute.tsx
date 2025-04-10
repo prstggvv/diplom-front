1aa1bcfb1f5382a25aa0cb3bfc1339427e753a7a
Ivan <vany4golos@yandex.ru>
2025-04-10 19:50:33 +0200

    feat: ProtectedRoute

---
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+import { ReactElement } from "react";
+
+interface ProtectedRouteProps {
+  element: ReactElement;
+  loggedIn: boolean;
+}
+
+function ProtectedRoute({
+  element, loggedIn,
+}: ProtectedRouteProps): ReactElement {
+  return loggedIn ? element : <Navigate to="/" replace />;
+}
+
+export default ProtectedRoute;
